dd378f5d4584bac6b3594b1cd3120851d5333962
Muhammad Ali <malitariq5324@gmail.com>
2025-08-31 00:21:23 +0500

    - Added TextField's Style Constant

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-const kTempTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 100.0,
-);
+const kTempTextStyle = TextStyle(fontFamily: 'Spartan MB', fontSize: 100.0);
 
-const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 60.0,
-);
+const kMessageTextStyle = TextStyle(fontFamily: 'Spartan MB', fontSize: 60.0);
 
-const kButtonTextStyle = TextStyle(
-  fontSize: 30.0,
-  fontFamily: 'Spartan MB',
-);
+const kButtonTextStyle = TextStyle(fontSize: 30.0, fontFamily: 'Spartan MB');
 
-const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+const kConditionTextStyle = TextStyle(fontSize: 100.0);
+
+const kTextFieldStyle = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  hintText: 'Enter City Name',
+  icon: Icon(Icons.location_city),
+  border: OutlineInputBorder(
+    borderSide: const BorderSide(width: 0.0, style: BorderStyle.none),
+    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+  ),
 );
