ae96efe396e6c28faf91f1a52d2ab964ea792c7c
Muhammad Ali <malitariq5324@gmail.com>
2025-08-31 18:13:30 +0500

    - Added the functionality of reading environment variables.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/screens/location_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -19,6 +20,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   getLocation() async {
+    await dotenv.load(fileName: ".env");
     weatherData = await WeatherModel().getLocationByCords();
     Navigator.push(
       context,
